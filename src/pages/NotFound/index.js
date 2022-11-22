f89f02566c892c97b36c991a6b4ddce1591e155d
eunhye210 <tkrhd0210@gmail.com>
2022-11-23 01:40:44 +0900

    feat: 404 page

---
+++ src/pages/NotFound/index.js
@@ -0,0 +1,18 @@
+import Header from "../../components/Header";
+
+import * as S from "./styles";
+
+function NotFound() {
+  return (
+    <S.NotFoundPageLayout>
+      <Header />
+      <S.Content>
+        <S.Message size="70">404 Page Not Found</S.Message>
+        <S.SubMesssage>The page you requested could not be found.</S.SubMesssage>
+        <S.SubMesssage>Please go back to the homepage.</S.SubMesssage>
+      </S.Content>
+    </S.NotFoundPageLayout>
+  )
+}
+
+export default NotFound;
